27ea908c5315bb823273522de6eee1a7b4c875dd
Hao <45501182+haobaids@users.noreply.github.com>
2024-07-03 23:25:48 +0000

    set up requirements file

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,31 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Test again! Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.text("This is some text")
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+
+st.code(code, language='python')
+
+import pandas as pd
+import numpy as np
+
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+st.dataframe(chart_data)
+st.line_chart(chart_data)
+
+# radio widget to take inputs from mulitple options
+genre = st.radio(
+    "What's your favorite movie genre",
+    ('Comedy', 'Drama', 'Documentary'))
+
+if genre == 'Comedy':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn't select comedy.")
+
